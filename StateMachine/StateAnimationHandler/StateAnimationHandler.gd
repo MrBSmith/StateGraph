a8b83d11ebea681d6d08d22a39c2b0bf303a6cba
Hugo Baba <Hugobarette@gmail.com>
2022-05-24 20:57:08 +0200

    StateAnimationHandler AnimationPlayer support Fix #7

--- StateMachine/StateAnimationHandler/StateAnimationHandler.gd
+++ StateMachine/StateAnimationHandler/StateAnimationHandler.gd
@@ -2,6 +2,11 @@ tool
 extends Node
 class_name StateAnimationHandler
 
+enum MODE {
+	ANIMATED_SPRITE,
+	ANIMATION_PLAYER
+}
+
 const DIRECTIONS_4 : Dictionary = {
 	"Up": Vector2.UP,
 	"Right": Vector2.RIGHT,
@@ -21,10 +26,14 @@ const DIRECTIONS_8 : Dictionary = {
 }
 
 onready var animated_sprite : AnimatedSprite = get_node_or_null(animated_sprite_path)
+onready var animation_player : AnimationPlayer = get_node_or_null(animation_player_path)
 onready var states_machine = get_parent()
 
+export var animation_player_path : NodePath
 export var animated_sprite_path : NodePath
 export var recursive_animation_triggering : bool = true
+
+export(MODE) var finished_trigger_mode : int = MODE.ANIMATED_SPRITE
 
 var object_direction := Vector2.ZERO
 
@@ -44,33 +53,39 @@ func _ready() -> void:
 	
 	var __ = get_parent().connect("state_entered", self, "_on_StateMachine_state_entered")
 	
-	if animated_sprite:
+	if animated_sprite && finished_trigger_mode == MODE.ANIMATED_SPRITE:
 		__ = animated_sprite.connect("animation_finished", self, "_on_animation_finished")
+	
+	elif animation_player && finished_trigger_mode == MODE.ANIMATION_PLAYER:
+		__ = animation_player.connect("animation_finished", self, "_on_animation_finished")
+		
+
 
 #### VIRTUALS ####
 
 
-func _update_animation(state: Node) -> void:
+func trigger_state_sound(state: State) -> void:
 	if state == null:
 		return
 	
 	var audio_stream_player = state.get_node_or_null("AudioStreamPlayer")
-	var state_name = state.name
-	var previous_state = states_machine.previous_state
-	var dir_name = find_dir_name(object_direction)
-	
 	if audio_stream_player != null:
 		audio_stream_player.stop()
 		audio_stream_player.play()
-	
-	if animated_sprite == null:
+
+
+func _update_sprite_animation(state: State) -> void:
+	if state == null or animated_sprite == null:
 		return
 
 	var sprite_frames = animated_sprite.get_sprite_frames()
 	if sprite_frames == null:
 		return
 	
-	var anim_name = state_name + dir_name
+	var dir_name = find_dir_name(object_direction)
+	var previous_state = states_machine.previous_state
+	
+	var anim_name = state.name + dir_name
 	var start_anim_name = "Start" + anim_name
 	var trans_anim_name = previous_state.name + "To" + anim_name if previous_state else ""
 
@@ -85,11 +100,32 @@ func _update_animation(state: Node) -> void:
 			animated_sprite.play(anim_name)
 
 
+func _update_animation_player(state: State) -> void:
+	if state == null or animation_player == null:
+		return
+	
+	var dir_name = find_dir_name(object_direction)
+	var previous_state = states_machine.previous_state
+	
+	var anim_name = state.name + dir_name
+	var start_anim_name = "Start" + anim_name
+	var trans_anim_name = previous_state.name + "To" + anim_name if previous_state else ""
+
+	if animation_player.has_animation(start_anim_name):
+		animation_player.play(start_anim_name)
+	
+	elif previous_state && animation_player.has_animation(trans_anim_name):
+		animation_player.play(trans_anim_name)
+
+	else:
+		if animation_player.has_animation(anim_name):
+			animation_player.play(anim_name)
+
 
 #### LOGIC ####
 
 # Find the name of the given direction and returns it as a String
-static func find_dir_name(dir: Vector2) -> String:
+func find_dir_name(dir: Vector2) -> String:
 	var dir_values_array = DIRECTIONS_8.values()
 	var dir_index = dir_values_array.find(dir)
 	
@@ -104,7 +140,7 @@ static func find_dir_name(dir: Vector2) -> String:
 
 #### SIGNAL RESPONSES #####
 
-func _on_animation_finished():
+func _on_animation_finished() -> void:
 	if animated_sprite == null:
 		return
 	
@@ -127,5 +163,6 @@ func _on_animation_finished():
 
 
 func _on_StateMachine_state_entered(new_state: Node) -> void:
-	_update_animation(new_state)
+	_update_sprite_animation(new_state)
+	_update_animation_player(new_state)
 
